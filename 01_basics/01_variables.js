693945dcaa75d459843a80c07670c39866aa8f1b
abhishekyadavweb <138349234+abhishekyadavweb@users.noreply.github.com>
2024-01-20 16:29:45 +0000

    config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14456
+let accountEmail = "abhi@google.com"
+var accountPassword = "abhi1111"
+accountCity = "bhadohi"
+
+// accountEmail = "a@b@"
+accountPassword = "asfd12"
+accountCity = 'boy'
+
+
+// accountId = 2
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+console.table([accountId, accountEmail, accountCity, accountPassword]);
