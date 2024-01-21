81551f94de46894e6979883de499a1408ac9691c
abhishekyadavweb <138349234+abhishekyadavweb@users.noreply.github.com>
2024-01-21 04:46:03 +0000

    conversion od datatypes

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = '125abc'
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let someNumber = 33
+let numberInString = String(someNumber)
+console.log(numberInString);
+console.log(typeof numberInString);
