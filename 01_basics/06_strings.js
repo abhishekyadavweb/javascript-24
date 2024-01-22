93d002384dd28159c28bed9e1d0f87b668540f85
abhishekyadavweb <138349234+abhishekyadavweb@users.noreply.github.com>
2024-01-22 17:42:45 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,8 @@
+const name = "abhi"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+console.log(`my name is ${name} and my repocount is ${50}`);
+
+const gameName = new String ("abhihc")
