8c7687ef730405521526430127de5477c39bded8
abhishekyadavweb <138349234+abhishekyadavweb@users.noreply.github.com>
2024-02-01 18:24:12 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,45 @@
+const user = {
+    username: "abhi",
+    price: "999",
+
+    welcomeMessage: function() {
+        console.log(`${this.username},  welcome to website`);
+    //   console.log(this);
+    }
+
+}
+// user.welcomeMessage()
+// user.username = "ram"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = 'abhi'
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function(){
+//     let username = 'abhi'
+//     console.log(this.username);
+// }
+const chai = () => {
+    let username = 'abhi'
+    console.log(this.username);
+    console.log(this);
+}
+// chai()
+
+
+
+// const addTwo = (num1, num2 ) => {
+//      return num1 + num2
+// }
+// const addTwo = (num1, num2 ) => (num1 + num2)
+// const addTwo = (num1, num2 ) => num1 + num2
+const addTwo = (num1, num2 ) => ({username: "abhi"})
+
+
+console.log( addTwo(5, 4));
