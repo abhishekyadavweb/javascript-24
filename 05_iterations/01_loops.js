a6bf1d80b8773fdf523db1399ac2a30986b4b814
abhishekyadavweb <138349234+abhishekyadavweb@users.noreply.github.com>
2024-02-03 18:12:34 +0000

    loops

---
+++ 05_iterations/01_loops.js
@@ -0,0 +1,46 @@
+// for
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+       // console.log("5 is best number");
+    }
+   // console.log(element);
+    
+}
+
+   // console.log(element);
+
+    for (let i = 0; i <= 10; i++) {
+        // console.log(`outer loop value: ${i}`);
+        for (let j = 0; j <= 10; j++) {
+            // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+           // console.log(i + '*' + j + ' = ' + i*j );
+        }
+        
+    }
+     let myArray = ["flash", "superman", "spiderman"]
+    //  console.log(myArray.length);
+    for (let index = 0; index < myArray.length; index) {
+        const element = myArray[index];
+        //console.log(element);
+    }
+
+    // break & continue
+
+    // for (let index = 0; index <=20; index++) {
+    //     if (index == 5) {
+    //         console.log(`detected 5`);
+    //         break
+    //     }
+    //     console.log(`value of i is ${index}`);
+        
+    // }
+    for (let index = 0; index <=20; index++) {
+        if (index == 5) {
+            console.log(`detected 5`);
+            continue
+        }
+        console.log(`value of i is ${index}`);
+        
+    }
